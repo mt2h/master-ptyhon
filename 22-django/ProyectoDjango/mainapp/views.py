c46011536c89d34e9876781adfe47cc81e062808
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2023-09-16 00:34:29 +0000

    example form

--- 22-django/ProyectoDjango/mainapp/views.py
+++ 22-django/ProyectoDjango/mainapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+#from django.contrib.auth.forms import UserCreationForm
+from mainapp.forms import RegisterForm
 
 # Create your views here.
-
 def index(request):
     return render(request, 'mainapp/index.html', {
         'title': 'Inicio'
@@ -10,4 +12,24 @@ def index(request):
 def about(request):
     return render(request, 'mainapp/about.html', {
         'title': 'Sobre nosotros'
+    })
+
+def register_page(request):
+
+    #register_form = UserCreationForm()
+    register_form = RegisterForm()
+
+    if request.method == 'POST':
+        #register_form = UserCreationForm(request.POST)
+        register_form = RegisterForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Te has registrado corrrectamente')
+
+            return redirect('inicio')
+
+    return render(request, 'users/register.html', {
+        'title': 'Registro',
+        'register_form': register_form
     })
